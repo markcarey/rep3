edb0bafa262226eeb535c65a1b4ab5c37bd8dc9d
markcarey <mark@markcarey.com>
2023-08-16 15:28:49 -0400

    other block scanners + opensea

--- rep3-chrome-extension/scripts/content.js
+++ rep3-chrome-extension/scripts/content.js
@@ -1,18 +1,18 @@
-
+var user = { "average": 4.6 };
+const stars = user.average;
 
 $( document ).ready(function() {
 
   $("#mainaddress").each(async function(){
     const address = $(this).text();
-    // TODO: fetch average rating
-    const res = await fetch(`https://api.rep3.bio/api/profile/${address}`, { 
-      method: 'GET', 
-      headers: {
-          "Content-Type": "application/json"
-      }
-    });
-    var user = await res.json();
-    const stars = user.average;
+    // fetch average rating
+    //const res = await fetch(`https://api.rep3.bio/api/profile/${address}`, { 
+    //  method: 'GET', 
+    //  headers: {
+    //      "Content-Type": "application/json"
+    //  }
+    //});
+    //var user = await res.json();
     const html = `
       <span id="ContentPlaceHolder1_rep3" style="margin-left: 0.75rem!important;">
         <a id="rep3" data-address="${address}" class="link-secondary" href="https://rep3.bio/profile/${address}" data-bs-toggle="tooltip" data-bs-trigger="hover" aria-label="Click to view Rep3 Profile">
@@ -20,9 +20,18 @@ $( document ).ready(function() {
         </a>
       </span>
     `;
-    $("#ContentPlaceHolder1_copyButtonPanel").after(html);
+    $("#ContentPlaceHolder1_copyButtonPanel").append(html);
   });
 
+  const opensea = $("section.item--counts").find("button:nth-child(2)").html();
+  if ( opensea ) {
+    $(".item--counts").find("button:last-child").after($("section.item--counts").find("button:nth-child(2)").clone());
+    $(".item--counts").find("button:last-child").css("margin-left", "24px").find("i").text("star_border").parents("button").find("span").text(`${stars} stars`);
+    $(".item--counts").find("button:last-child").click(function(){
+      const path = window.location.pathname.split('/');
+      window.location = `https://rep3.bio/nft/${path[2]}/${path[3]}/${path[4]}`;
+    });
+  }
 
 
 });
